36539ab3dc727eae9fcd140a01d144757311bab3
darshanlal2k <darshanlal2k@gmail.com>
2023-11-29 16:21:02 +0530

    validation testing

--- registrationform/src/Components/Regform.js
+++ registrationform/src/Components/Regform.js
@@ -1,20 +1,23 @@
 import { Box, Typography, TextField, Button, Stack } from "@mui/material";
-import React, { useState } from "react";
+import React, { } from "react";
 import { TextareaAutosize } from '@mui/base/TextareaAutosize';
+import { useForm } from "react-hook-form";
 
 
 export default function Regform() {
+    const { register, handleSubmit, formState: { errors } } = useForm()
 
-    const [info, setInfo] = useState();
+    // const [info, setInfo] = useState();
     return (
-        <div className="form">
-            <div>
-                <p></p>
-            </div>
-            <form>
+        <div>
+            <form onSubmit={handleSubmit(
+                (data) => {
+                    console.log(data);
+                }
+            )}>
                 <Box display={"flex"} flexDirection={"column"} maxWidth={400}
                     alignItems={"center"} justifyContent={"center"} margin={"auto"}
-                    marginTop={1} borderRadius={3}
+                    marginTop={1} marginBottom={2} borderRadius={3}
                     boxShadow={"5px 5px 10px #ccc"}
                     sx={{
                         ":hover": {
@@ -22,47 +25,67 @@ export default function Regform() {
                         }
                     }} >
                     <Typography variant="h4" padding={2} textAlign={"center"}> Registration Form</Typography>
-                    <Stack>
-                        <TextField
+                    <Stack marginBottom={5}>
+                        <TextField {...register('name', {
+                            required: 'name is required',
+                            minLength: {
+                                value: 3,
+                                message: 'name should atleast minimum 3 characters',
+                            },
+                            maxLength: {
+                                value: 10,
+                                message: 'name should maximum 25 characters'
+                            },
+                        })}
+                            // helperText={errors.name && errors.name.message}
+                            // error={Boolean(errors.message)}
                             margin="normal"
                             placeholder="name"
                             type="text"
                             variant="outlined"
-                            required
+                            
                             label="name"
-                            value={info}
-                            onChange={
-                                (e) => setInfo(e.target.value)
-                            }
-                            helperText={!info ? "Please Enter Your Name" : ""}
-                            error={!info}
-
                         />
+                         {errors?.name && <p style={{ color: 'red' }}>{errors.name.message}</p>} 
+                        <TextField {...register('email', { required: 'email is required' })}
+                            margin="normal"
+                            placeholder="email"
+                            type="email"
+                            variant="outlined"
+                            
+                            label="email" />
+                        <TextField {...register('company name', { required: true })}
+                            margin="normal"
+                            placeholder="company name"
+                            type="text"
+                            variant="outlined"
+                            
+                            label="company name" />
+                        <TextField {...register('contact', { required: true })}
+                            margin="normal"
+                            placeholder="contact"
+                            type="text"
+                            variant="outlined"
+                            
+                            label="contact" />
+                        <TextareaAutosize {...register('comments', { required: true })}
+                            type="string"
+                            minRows={3}
+                            placeholder="comments"
+                    
+                            ariant="outlined"
+                            label="comments"
+                            sx={{
+                                mt: 2
+                            }}
+                        >
+                        </TextareaAutosize>
+                        {/* <Textarea aria-label="minimum height" minRows={3} placeholder="Minimum 3 rows" /> */}
+                        <Button variant="contained" sx={{ marginTop: 2 }} type="submit" >Submit</Button>
                     </Stack>
 
-                    <TextField margin="normal" placeholder="email" type="email" variant="outlined" />
-                    <TextField margin="normal" placeholder="company name" type="text" variant="outlined" />
-                    <TextField margin="normal" placeholder="contact" type="text" variant="outlined" />
-                    <TextareaAutosize type="string" minRows={5}></TextareaAutosize>
-                    {/* <Textarea aria-label="minimum height" minRows={3} placeholder="Minimum 3 rows" /> */}
-                    <Button variant="contained" sx={{ marginTop: 2 }}>Submit</Button>
-                    <Button sx={{ marginTop: 2, borderRadius: 1 }}>Login</Button>
+
                 </Box>
-                {/* <div>
-                    <label className="label">Username</label>
-                    <input type="text" className="input" />
-                </div>
-                <div>
-                    <label className="label">Email</label>
-                    <input type="email" className="input" />
-                </div>
-                <div>
-                    <label className="label">Password</label>
-                    <input type="password" className="input" />
-                </div>
-                <div>
-                    <button type="submit" className="btn">Submit</button>
-                </div> */}
             </form>
         </div>
     );
